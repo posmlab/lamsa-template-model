4d85882df7535c8440015a41eca7012dcaae8130
rosedidcock <60052924+rosedidcock@users.noreply.github.com>
2020-06-12 12:40:10 -0700

    fixed friction

--- unlatching_ode.m
+++ unlatching_ode.m
@@ -5,7 +5,7 @@ y=y_L{1}(x(1))+y0;
 yL_prime = y_L{2}(x(1));
 yL_doubleprime = y_L{3}(x(1));
 Y=[y,yL_prime*x(2)];
-denom=m_L+m_eff*yL_prime^2+m_L*coeff_fric*yL_prime-m_eff*yL_prime*coeff_fric;
-num=F_out(t,x)+coeff_fric*F_out(t,x)*yL_prime+yL_prime*F_s(t,y)-F_s(t,y)*coeff_fric-m_eff*yL_prime*yL_doubleprime*x(2)^2+m_eff*coeff_fric*yL_doubleprime*x(2)^2;
+denom=m_L+m_eff*(yL_prime^2)+coeff_fric*(m_L*yL_prime-m_eff*yL_prime);
+num=F_out(t,x)+yL_prime*F_s(t,y)+coeff_fric*(F_out(t,x)*yL_prime-F_s(t,y))-(x(2)^2)*(m_eff*yL_prime*yL_doubleprime-m_eff*coeff_fric*yL_doubleprime);
 dx=[x(2);num/denom];
 end
