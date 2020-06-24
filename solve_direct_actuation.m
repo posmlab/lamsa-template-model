c6f4e3ba8ae6ac974831b9c06c4b0f182542a9bf
rosedidcock <60052924+rosedidcock@users.noreply.github.com>
2020-06-24 14:01:16 -0700

    editting model

--- solve_direct_actuation.m
+++ solve_direct_actuation.m
@@ -13,7 +13,7 @@ function sol = solve_direct_actuation(motor,load)
 %% Ballistic phase:F motor only 
 launch_opts=odeset('Events',@(t,y) direct_actuation_end(t,y,motor));
 ode=@(t,y) direct_actuation_ode(t,y,load,motor);
-tspan=linspace(0,10,1000);
+tspan=linspace(0,5,1000);
 y0=[0,0];
 [t,y]=ode45(ode,tspan,y0,launch_opts);
 T=[t];
@@ -22,9 +22,5 @@ for i = 1:size(T)
     fMotor(i) = motor.Force(T(i), [Y(i,:)]);
 end
 fMotor = fMotor';
-index=find(fMotor==0.0000,1,"first")
-fMotor = fMotor(1:index,1);
-T=T(1:index,1);
-Y=Y(1:index,:);
 
-sol=[T,Y,fMotor]
+sol=[T,Y,fMotor];
