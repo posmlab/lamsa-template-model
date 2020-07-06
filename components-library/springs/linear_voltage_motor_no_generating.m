dd20db40ab203d936effa8bbc28b16a342a0cfa4
Mason Acevedo <macevedo@g.hmc.edu>
2020-07-06 14:20:42 -0700

    added voltage motors

---
+++ components-library/springs/linear_voltage_motor_no_generating.m
@@ -0,0 +1,38 @@
+%% linear_motor struct
+% NOTE: this is a weird motor, in which 
+%       there is a linear Force vs Velocity tradeoff, 
+%       except that when the velocity of the motor exceeds its 
+%       maximum velocity, it 
+% arguments in required order:
+%     F_motor_max_max  - maximum amount of force the spring can exert AT MAX
+%                        VOLTAGE, i.e. voltage_fraction = 1 the F_motor_max 
+%                        will get scaled down w/ the voltage fraction
+%     v_motor_max_max  - maximum velocity at which the motor can travel AT MAX
+%                        VOLTAGE, i.e. voltage_fraction = 1 the F_motor_max 
+%                        will get scaled down w/ the voltage fraction 
+%     range_of_motion  - how far the motor can contract
+%     voltage_fraction - the fraction of the max voltage at which the motor
+%                        is being powered - the motor's maxmax force and
+%                        maxmax velocity get scaled by this number,
+%                        resulting in a new Force vs Velocity 'curve' (its
+%                        just linear)
+% min # arguments = 4
+
+
+function motor = linear_voltage_motor_no_generating(F_motor_max_max, v_motor_max_max, range_of_motion, voltage_fraction)
+
+if (nargin < 4)
+    error("voltage_motor requires you input a voltage_fraction, representing the fraction of the motor's max voltage")
+end
+
+motor.max_force = voltage_fraction*F_motor_max_max;
+motor.range = range_of_motion;
+motor.velocity = voltage_fraction*v_motor_max_max;
+
+if x(2) <= motor.velocity
+    motor.Force = @(t,x)(motor.max_force*(1-x(2)/motor.velocity)) .* (abs(x(1))<=range_of_motion);
+else
+    motor.Force = 0
+end
+
+end
