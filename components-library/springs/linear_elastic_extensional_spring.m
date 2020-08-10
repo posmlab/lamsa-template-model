67ae0e011536a5af20b527854ab53c55e99f785d
milton-hmc <61558639+milton-hmc@users.noreply.github.com>
2020-08-10 10:01:00 -0700

    Update linear_elastic_extensional_spring.m

--- components-library/springs/linear_elastic_extensional_spring.m
+++ components-library/springs/linear_elastic_extensional_spring.m
@@ -5,26 +5,34 @@
 %     L - length
 %     rho - density (optional)
 %     sigma_f - failure strength in Pascals (optional)
-% min # arguments = 1
+% min # arguments = 0
 
-function spring = linear_elastic_extensional_spring(E,A,L,varargin)
-    varargin_param_names = {'rho','sigma_f'};
-    varargin_default_values = {0,Inf};
+function spring = linear_elastic_extensional_spring(varargin)
+    %% linear_elastic_extensional_spring input parameters
+    spring.param_names = {'E','A','L','rho','sigma_f'};
+    spring.param_default_values = {1,1,1,0,Inf};
+    
+    %% General code for spring components
+    spring.param_values = containers.Map(spring.param_names,spring.param_default_values,'UniformValues',false);
     
     % check and assign optional parameters
-    if (nargin < 1)
-        error('Linear elastic extensional spring requires at least 1 argument.');
-    end
-    if (length(varargin)>length(varargin_param_names))
+    if (length(varargin)>length(spring.param_names))
         error('Too many input parameters');
     end
+    
+    % loop over input arguments and assign them to spring.param_values container Map
     for i=1:length(varargin)
-        eval([varargin_param_names{i} '=varargin{i};'])
+        spring.param_values(spring.param_names{i}) = varargin{i};
     end
-    for i=(length(varargin)+1):length(varargin_param_names)
-        eval([varargin_param_names{i} '=varargin_default_values{i};'])
-    end
+    
+    % loop over parameter names and store them outside the container map
+    % (making a local copy to make their use in any equations more human
+    % readable)
+    for i=1:length(spring.param_names)
+        eval([spring.param_names{i} ' = spring.param_values(''' spring.param_names{i} ''')';]);
+    end    
 
+    %% Defining the linear_elastic_extensional_spring Force, mass, and range
     k = (E*A)/L;
     m = rho*A*L;
     F_spring_max = sigma_f*A;
