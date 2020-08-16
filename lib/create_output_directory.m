b27adea2db6fb05a3bab839d593354af99a6c89c
milton-hmc <61558639+milton-hmc@users.noreply.github.com>
2020-08-15 19:52:16 -0700

    Update create_output_directory.m
    now creates the directory if it doesn't already exist

--- lib/create_output_directory.m
+++ lib/create_output_directory.m
@@ -1,5 +1,12 @@
 function output_directory = create_output_directory()
+curr_dir = pwd;
 dateString = string(datetime);
 cleanDateString = regexprep(dateString, " ", "_");
 cleanDateString = regexprep(cleanDateString, ":", "_");
 output_directory = fullfile('../output',cleanDateString);
+if (~exist(fullfile(output_directory),'file'))
+    mkdir(fullfile(output_directory));
+end
+cd(output_directory);
+output_directory = pwd;
+cd(curr_dir);
