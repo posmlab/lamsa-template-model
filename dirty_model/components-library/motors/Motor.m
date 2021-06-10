f7cba763e36ac34865f2067ff1be85fff63c5184
Declan O'Neill <78057886+doneill890@users.noreply.github.com>
2021-06-10 14:56:26 -0700

    fresh start

---
+++ dirty_model/components-library/motors/Motor.m
@@ -0,0 +1,28 @@
+%% Motor class definition
+
+% Motor is a superclass for motor objects
+
+% subclass structure:
+%
+% Motor
+%       LinearMotor
+%       HillMuscleMotor
+
+classdef Motor
+    
+    % common properties for all types of motors
+    properties
+        max_force, range, velocity, Force
+    end
+    
+    methods
+        % constructor
+        function obj = Motor(max_force, range, velocity, Force)
+            obj.Force  = Force;
+            obj.velocity = velocity;
+            obj.range  = range;
+            obj.max_force = max_force;
+         end
+    end
+     
+end
