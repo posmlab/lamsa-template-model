6823f5645d91f9126dd6d4d82c6f844bbd676f90
nothepburn <84350065+nothepburn@users.noreply.github.com>
2021-07-28 10:01:40 -0700

    most recent motor

--- components-library/motors/Subclasses/TwoPartMuscleMotor.m
+++ components-library/motors/Subclasses/TwoPartMuscleMotor.m
@@ -6,25 +6,16 @@
 % doi:10.1007/s00422-007-0160-6.
 %
 % arguments in required order:
-%     range- range of motion
+
 %     L_0 - rest length of the contractile element
 %     v_motor_max - maximum velocity at which the motor can travel
 %     F_max - F_max in [N] for Extensor
 %     l_CEopt - optimal length of CE in [m] for Extensor
-%    L_PEE0 - rest length of PEE normalized to optimal lenght of CE (Guenther et al., 2007)
-%    v_PEE -  exponent of F_PEE (Moerl et al., 2012)
-%    F_PEE - force of PEE if l_CE is stretched to deltaWlimb_des (Moerl et al., 2012)
+%     L_PEE0 - rest length of PEE normalized to optimal lenght of CE (Guenther et al., 2007)
+
 %optional
-%     q - muscles activity, 0<=q<=1
-%     DeltaW_limb_des - width of normalized bell curve in descending branch
-%     of force-length relationship
-%     DeltaW_limb_asc - width of normalized bell curve in ascending branch
-%     of force length relationship
-%     A_rel0 - parameter for contraction dynamics: maximum value of A_rel
-%    B_rel0 -  parameter for contraction dynmacis: maximum value of B_rel
-%    S_eccentric - relation between F(v) slopes at v_CE=0 (van Soest & Bobbert, 1993)
-%    F_eccentric - factor by which the force can exceed F_isom for large eccentric velocities (van Soest & Bobbert, 1993)
-%       min # arguments = 15
+%     L_initial - initial stretch on the contractile element  
+%       min # arguments = 5
 
 classdef TwoPartMuscleMotor < Motor
     
@@ -33,24 +24,26 @@ classdef TwoPartMuscleMotor < Motor
         % second row contains default values for the loading motor
         % third row contains default values for the unlatching motor
         function parameters = parameters()
-            parameters = [ "range" "L_0" "v_motor_max" "F_max" "l_CEopt"  "L_PEE0" "v_PEE" "F_PEE"...
-                           "q" "DeltaW_limb_des" "DeltaW_limb_asc" "v_CElimb_des" "v_CElimb_asc" "A_rel0" "B_rel0" "S_eccentric" "F_eccentric";
-                "10" "0.0828" "1" "1420" "0.092" "0.9" "2.5" "2.0" "1" "0.35" "0.35" "1.5" "3" "0.25" "2.25" "2" "1.5";
-                "10" "0.0828" "1" "1420" "0.092" "0.9" "2.5" "2.0" "1" "0.35" "0.35" "1.5" "3" "0.25" "2.25" "2" "1.5";
-                "0" "0" "0" "0" "0" "0" "0" "0" "0" "0" "0" "0" "0" "0" "0" "0" "0";
-                "Inf" "Inf" "Inf" "Inf" "Inf" "Inf" "Inf" "Inf" "1" "Inf" "Inf" "Inf" "Inf" "Inf" "Inf" "Inf" "Inf"];
+            parameters = [ "L_0" "v_motor_max" "F_max" "l_CEopt"  "L_PEE0"...
+                "L_initial";
+                 "0.0828" "1" "5" "0.092" "0.9" "0";
+                 "0.0828" "1" "5" "0.092" "0.9" "0";
+                 "0" "0" "0" "0" "0" "0";
+                 "Inf" "Inf" "Inf" "Inf" "Inf" "Inf"];
         end
     end
     
     methods
-        function obj = TwoPartMuscleMotor( range, L_0, v_motor_max, F_max, l_CEopt, L_PEE0, v_PEE, F_PEE, varargin)
+        function obj = TwoPartMuscleMotor( L_0, v_motor_max, F_max, l_CEopt, L_PEE0, varargin)
+            
             % optional parameters
+            varargin_param_names = {'L_initial'};
+            varargin_default_values = {0};
            % 
-            varargin_param_names = {'q' 'DeltaW_limb_des' 'DeltaW_limb_asc' 'v_CElimb_des' 'v_CElimb_asc' 'A_rel0' 'B_rel0' 'S_eccentric' 'F_eccentric'};
-            varargin_default_values = {1, 0.35, 0.35, 1.5, 3, 0.25, 2.25, 2, 1.5};
-            % check and assign optional parameters
-            if (nargin < 8)
-                error('Two Part muscle motor requires at least 8 arguments.');
+             
+%             % check and assign optional parameters
+            if (nargin < 4)
+                error('Two Part muscle motor requires at le%ast 4 arguments.');
             end
             
             if (length(varargin)>length(varargin_param_names))
@@ -68,16 +61,37 @@ classdef TwoPartMuscleMotor < Motor
                 warning("v_max argument must be nonzero, setting to Inf")
             end
             % model
+            % constants
+            v_PEE=2.5;%    v_PEE -  exponent of F_PEE (Moerl et al., 2012)
+            F_PEE=2.0;%    F_PEE - force of PEE if l_CE is stretched to deltaWlimb_des (Moerl et al., 2012)
+            %DeltaW_limb_des - width of normalized bell curve in descending branch of force-length relationship
+            DeltaW_limb_des=0.35;
+            % DeltaW_limb_asc - width of normalized bell curve in ascending branch of force length relationship
+            DeltaW_limb_asc=0.35;
+            %A_rel0 - parameter for contraction dynamics: maximum value of A_rel
+            A_rel0=0.25;
+            %    B_rel0 -  parameter for contraction dynmacis: maximum value of B_rel
+            B_rel0=2.25;
+            %    S_eccentric - relation between F(v) slopes at v_CE=0 (van Soest & Bobbert, 1993)
+            S_eccentric=2;
+            %    F_eccentric - factor by which the force can exceed F_isom for large eccentric velocities (van Soest & Bobbert, 1993)
+            F_eccentric=1.5;
+            % exponent for descending branch (Moerl et al., 2012)
+            v_CElimb_des = 1.5; 
+            % exponent for ascending branch (Moerl et al., 2012)
+            v_CElimb_asc = 3.0;
             
             %changing variables
-            l_CE=@(t,x) x(1)+L_0;
+            l_CE=@(t,x) L_0-x(1);
             dot_l_CE=@(t,x) x(2);
-            
+                %muscles activity, 0<=q<=1
+            r_activation=200;
+            q=@(t,x)min(r_activation*t,1);
             %force calculations
             %chill initial stuff
             l_PEE0 = L_PEE0*l_CEopt;       % rest length of PEE (Guenther et al., 2007)
            
-            K_PEE= F_PEE*( F_max/ ( l_CEopt*(DeltaW_limb_des+1-L_PEE0) )^v_PEE );
+            K_PEE= F_PEE*( F_max/ ( l_CEopt*(DeltaW_limb_des+1-L_PEE0) )^v_PEE );%I think I have a units issue here??? This is the same equation so...
             % Isometric force (Force length relation)
             %Guenther et al. 2007
           %descending branch or ascending branch
@@ -85,25 +99,27 @@ classdef TwoPartMuscleMotor < Motor
 
                                         % factor of non-linearity in F_PEE (Guenther et al., 2007)
             % Force of the parallel elastic element
-                F_PEE =@(t,x) K_PEE*(l_CE(t,x)-l_PEE0)^(v_PEE)*(l_CE(t,x) >= l_PEE0);
+            F_PE =@(t,x) K_PEE*(l_CE(t,x)-l_PEE0)^(v_PEE)*(l_CE(t,x) >= l_PEE0);
        
             
             % Hill Parameters concentric contraction
             A_rel=@(t,x)1*(l_CE(t,x)<l_CEopt)+ (l_CE(t,x)>=1)*(F_isom(t,x));
             
-            A_rel = @(t,x) (A_rel(t,x) * A_rel0*1/4*(1+3*q))*(dot_l_CE(t,x)<=0)+(dot_l_CE(t,x) > 0)*(-F_eccentric*q*F_isom(t,x));
+            A_rel = @(t,x) (A_rel(t,x) * A_rel0*1/4*(1+3*q(t,x)))*(dot_l_CE(t,x)<=0)+(dot_l_CE(t,x) > 0)*(-F_eccentric*q(t,x)*F_isom(t,x));
+            
+            B_rel = @(t,x)B_rel0*1*1/7*(3+4*q(t,x));
 
-            B_rel = B_rel0*1*1/7*(3+4*q);
-
-            B_rel=@(t,x) B_rel*(dot_l_CE(t,x)<=0)+(dot_l_CE(t,x) > 0)*((q*F_isom(t,x)*(1-F_eccentric)/(q*F_isom(t,x)+A_rel(t,x))*B_rel/S_eccentric));
+            B_rel=@(t,x) B_rel(t,x)*(dot_l_CE(t,x)<=0)+(dot_l_CE(t,x) > 0)*((q(t,x)*F_isom(t,x)*(1-F_eccentric)/(q(t,x)*F_isom(t,x)+A_rel(t,x))*B_rel(t,x)/S_eccentric));
             
             % Contractile element force (isometric)
             % I think this might just be to initialize F_CE = F_max*q*F_isom;
-            F_CE = @(t,x) F_max*(( (q*F_isom(t,x)+A_rel(t,x)) / (1 - dot_l_CE(t,x)/(l_CEopt*B_rel(t,x)) ) )-A_rel(t,x));
+            F_CE = @(t,x) F_max*(( (q(t,x)*F_isom(t,x)+A_rel(t,x)) / (1 - dot_l_CE(t,x)/(l_CEopt*B_rel(t,x)) ) )-A_rel(t,x));
             %needed for constructor
-            Force=@(t,x)F_CE(t,x)+F_PEE(t,x);
+            Force=@(t,x)F_CE(t,x)+F_PE(t,x);
             max_force = F_max;
-            range=range;
+            
+            range=0.3*L_0-L_initial;
+            %it needs to mean stretch minus 0.7 rest length. What's initial stretch?
         %     motor.range=muscle_length;
             velocity=@(t,x) dot_l_CE;
             
