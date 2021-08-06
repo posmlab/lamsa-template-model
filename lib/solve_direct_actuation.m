e18ccecf42e576c8debd51cf76a88e4ba7731588
Declan O'Neill <78057886+doneill890@users.noreply.github.com>
2021-08-06 14:53:24 -0700

    updating code for new components

--- lib/solve_direct_actuation.m
+++ lib/solve_direct_actuation.m
@@ -13,8 +13,8 @@ function [sol, transition_times] = solve_direct_actuation(motor,load)
 launch_opts=odeset('Events',@(t,y) direct_actuation_end(t,y,motor),'RelTol',1E-7,'AbsTol',1E-10);
 ode=@(t,y) direct_actuation_ode(t,y,load,motor);
 
-t_guess_v=(motor.velocity*load.mass([1,1]))/motor.max_force;
-t_guess_pos=sqrt((2*motor.range*load.mass([1,1]))/motor.max_force);
+t_guess_v=(motor.velocity*load.mass)/motor.max_force;
+t_guess_pos=sqrt((2*motor.range*load.mass)/motor.max_force);
 
 t_guess=max(t_guess_v,t_guess_pos);
 tspan=linspace(0,t_guess,1000);
@@ -38,3 +38,56 @@ fMotor = fMotor';
 
 transition_times = [T(1), T(end)];
 sol=[T,Y,fMotor];
+end
+
+function dy=direct_actuation_ode(t,y,load,motor)
+%ODE for direct actuation: a=F/m
+dy=zeros(2,1);
+dy(1)=y(2);
+
+Force = F_eff(load, motor, t, y);
+mass = m_eff(load, y);
+
+dy(2)=Force/mass;
+
+end
+
+function [value,isterminal,direction]=direct_actuation_end(t,y,motor)
+%End condition for loading
+value=motor.Force(t,y);
+isterminal=1;
+direction=0;
+end
+
+function mass = m_eff(load, y)
+
+mass = load.mass;
+
+if isa(load, 'RotatingMass')
+    L1 = load.L1;
+    theta_0 = load.theta_0;
+    theta = asin((-y(1)+L1*sin(theta_0))/L1);
+    mass = mass/cos(theta) + 1E2*(abs(theta) > pi/2);
+end
+
+end
+
+function Force = F_eff(load, motor, t, y)
+
+Force = motor.Force(t, y);
+
+if isa(load, 'RotatingMass')
+    
+    y1 = motor.rest_length;
+    y2 = motor.rest_length - y(1);
+    theta_0 = load.theta_0;
+    L1 = load.L1;
+    m = load.mass;
+    
+    theta = asin((-y(1)+L1*sin(theta_0))/L1);
+    alpha = acos((y2^2-y1^2+2*y1*L1*sin(theta_0))/(2*y2*L1));
+    
+    Force = real( Force*sin(alpha) - abs(m*sin(theta)*y(2)^2/(cos(theta)^3))) * (abs(theta) < pi/2);
+end
+
+end
--- lib/solve_model.m
+++ lib/solve_model.m
@@ -69,10 +69,10 @@ if isa(unlatching_motor, 'DeactivatingMotor')
     mu = latch.coeff_fric;
     numer = mu*cos(angle)-sin(angle);
     denom = cos(angle)+mu*sin(angle);
-    new_max = spring.Force(0, y0)*numer/denom;
+    new_max = F_eff(load, spring, y0, Inf, [y0,0])*numer/denom;
+    condition = new_max < unlatching_motor.max_force; %forces are both negative
     unlatching_motor.max_force = new_max;
     unlatching_motor.Force = @(t,x) new_max * exp(-unlatching_motor.r_deactivation*t);
-    condition = false; % we just found the necessary force to hold the latch
 else
     condition = latch.coeff_fric < latch.y_L{2}(0);
 end
@@ -103,7 +103,7 @@ if (unlatching_motor.max_force==0 && latch.v_0 == 0)
     return
 end
 try
-    [inst_check,~,~]=unlatching_end(0,[0,latch.v_0],@(y)m_eff(load,spring,y0,y),y0,latch,spring,unlatching_motor);
+    [inst_check,~,~]=unlatching_end(0,[0,latch.v_0],load,y0,latch,spring,unlatching_motor);
 catch ME
     switch ME.message
         case 'Latch gets stuck!'
@@ -121,8 +121,9 @@ catch ME
     end
 end
 if inst_check>0  % not instantaneous unlatching
-    unlatch_opts=odeset('Events',@(t,y) unlatching_end(t,y,@(y)m_eff(load,spring,y0,y),y0,latch,spring,unlatching_motor),'RelTol',1E-6,'AbsTol',1E-9);
-    ode=@(t,y) unlatching_ode(t,y,@(y)m_eff(load,spring,y0,y),y0,latch,spring,unlatching_motor);
+    unlatch_opts=odeset('Events',@(t,y) unlatching_end(t,y,load,y0,latch,spring,unlatching_motor),...
+        'RelTol',1E-6,'AbsTol',1E-9,'OutputFcn', @(t,y,flag) update_f_unlatching(t,y,flag,spring,latch,y0));
+    ode=@(t,y) unlatching_ode(t,y,load,y0,latch,spring,unlatching_motor);
     
     a_0L = abs(unlatching_motor.max_force / latch.mass);
     
@@ -198,7 +199,7 @@ y_unlatch = real(y_unlatch);
 F_n = zeros(size(x_unlatch, 1),1);
 for i=1:size(x_unlatch, 1)% For derivation of this equation for F_n see Overleaf doc with LaMSA derivation
     y=(latch.y_L{1}(x_unlatch(i,1))-latch.y_L{1}(0))+y0;
-    num1 = (latch.mass*spring.Force(t_unlatch(i), y_unlatch(i, :))) - ...
+    num1 = (latch.mass*F_eff(load,spring,y0,t_unlatch(i), y_unlatch(i, :))) - ...
         (m_eff(load,spring,y0,y)*latch.y_L{3}(x_unlatch(i,1))*(x_unlatch(i,2)^2)*latch.mass) - ...
         (unlatching_motor.Force(t_unlatch(i), x_unlatch(i,:))*m_eff(load,spring,y0,y)*latch.y_L{2}(x_unlatch(i,1)));
     rad = 1 + ((latch.y_L{2}(x_unlatch(i,1)))^2);
@@ -240,11 +241,11 @@ F_comp = [F_nx F_ny F_fx F_fy];
 stiffness = abs(( spring.Force(0,y_unlatch(end,:))-spring.Force(0,y_unlatch(end,:)+(100*eps)) ) / (100*eps)); 
 nat_freq=sqrt(stiffness/m_eff(load,spring,y0,y_unlatch));
 t_launch_guess=pi/nat_freq;
-launch_opts=odeset('Events',@(t,y) launching_end(t,y,spring));
-ode=@(t,y) launching_ode(t,y,m_eff(load,spring,y0,y),spring);
+launch_opts=odeset('Events',@(t,y) launching_end(t,y,load,spring,y0),'OutputFcn', @(t,y,flag) update_f_launching(t,y,flag,spring));
+ode=@(t,y) launching_ode(t,y,load,spring,y0);
 tspan=linspace(0,t_launch_guess,1E3);
-y0=y_unlatch(end,:)';
-[t_launch,y_launch]=ode45(ode,tspan,y0,launch_opts);
+y_init=y_unlatch(end,:)';
+[t_launch,y_launch]=ode45(ode,tspan,y_init,launch_opts);
 
 %% Stitch together solutions
 T=[t_unlatch;t_unlatch(end)+t_launch];
@@ -268,7 +269,7 @@ end
 fSpring = zeros(size(T,1),1);
 fUnlatchingMotor = zeros(size(T,1),1);
 for i = 1:size(T)
-    fSpring(i) = spring.Force(T(i), [Y(i,1), Y(i,2)]);%fill out the fSpring vector to add to sol
+    fSpring(i) = F_eff(load,spring,y0,T(i),[Y(i,1),Y(i,2)]);%fill out the fSpring vector to add to sol
     if (T(i) < t_unlatch(end))
         fUnlatchingMotor(i) = unlatching_motor.Force(T(i), X(i,:));
     else
@@ -297,34 +298,37 @@ end
 
 end
 
-function dx = launching_ode(t,x,m_eff,spring)
+function dx = launching_ode(t,y,load,spring,y0)
 %ODE for the launching phase
-dx(1)=x(2,1);
-dx(2)=spring.Force(t,x)/m_eff;
+dx(1)=y(2,1);
+dx(2)=F_eff(load, spring, y0, t, y)/m_eff(load, spring, y0, y(1));
 dx=dx';
 end
 
-function [value,isterminal,direction] = launching_end(t,y,spring)
-%End condition for loading
-value=spring.Force(t,y);
+function [value,isterminal,direction] = launching_end(t,y,load,spring,y0)
+%End condition for launching
+value=F_eff(load, spring, y0, t, y);
 isterminal=1;
 direction=0;
 end
 
-function dx = unlatching_ode(t,x,m_eff_fun,y0,latch,spring,unlatching_motor)
+function dx = unlatching_ode(t,x,load,y0,latch,spring,unlatching_motor)
 % ODE for the unlatching phase
 y=(latch.y_L{1}(x(1))-latch.y_L{1}(0))+y0;
-m_eff = m_eff_fun(y);
+m = m_eff(load, spring, y0, y);
 yL_prime = latch.y_L{2}(x(1));
 yL_doubleprime = latch.y_L{3}(x(1));
 Y=[y,yL_prime*x(2)];
 scaling = sign(x(2));
-denom=latch.mass+m_eff*(yL_prime^2)+scaling*latch.coeff_fric*(latch.mass*yL_prime-m_eff*yL_prime);
-num=unlatching_motor.Force(t,x)+yL_prime*spring.Force(t,y)+scaling*latch.coeff_fric*(unlatching_motor.Force(t,x)*yL_prime-spring.Force(t,y))-(x(2)^2)*(m_eff*yL_prime*yL_doubleprime-m_eff*scaling*latch.coeff_fric*yL_doubleprime);
+
+spring_force = F_eff(load, spring, y0, t, Y);
+
+denom=latch.mass+m*(yL_prime^2)+scaling*latch.coeff_fric*(latch.mass*yL_prime-m*yL_prime);
+num=unlatching_motor.Force(t,x)+yL_prime*spring_force+scaling*latch.coeff_fric*(unlatching_motor.Force(t,x)*yL_prime-spring_force)-(x(2)^2)*(m*yL_prime*yL_doubleprime-m*scaling*latch.coeff_fric*yL_doubleprime);
 dx=[x(2);num/denom];
 end
 
-function [value,isterminal,direction] = unlatching_end(t,x,m_eff_fun,y0,latch,spring,unlatching_motor)
+function [value,isterminal,direction] = unlatching_end(t,x,load,y0,latch,spring,unlatching_motor)
 
 %End condition for unlatching
 if (x(1) > latch.max_width)
@@ -338,15 +342,16 @@ yL=(latch.y_L{1}(x(1))-latch.y_L{1}(0))+y0;
 yL_prime = latch.y_L{2}(x(1));
 yL_doubleprime = latch.y_L{3}(x(1));
 y=[yL yL_prime*x(2)];
-m_eff = m_eff_fun(yL);
+m = m_eff(load, spring, y0, y);
+spring_force = F_eff(load, spring, y0, t, y);
 frictionDirection = sign(x(2));
-denom=latch.mass+m_eff*(yL_prime^2)+frictionDirection*latch.coeff_fric*(latch.mass*yL_prime-m_eff*yL_prime);
-num=unlatching_motor.Force(t,x)+yL_prime*spring.Force(t,y)+frictionDirection*latch.coeff_fric*(unlatching_motor.Force(t,x)*yL_prime-spring.Force(t,y))-(x(2)^2)*(m_eff*yL_prime*yL_doubleprime-m_eff*frictionDirection*latch.coeff_fric*yL_doubleprime);
+denom=latch.mass+m*(yL_prime^2)+frictionDirection*latch.coeff_fric*(latch.mass*yL_prime-m*yL_prime);
+num=unlatching_motor.Force(t,x)+yL_prime*spring_force+frictionDirection*latch.coeff_fric*(unlatching_motor.Force(t,x)*yL_prime-spring_force)-(x(2)^2)*(m*yL_prime*yL_doubleprime-m*frictionDirection*latch.coeff_fric*yL_doubleprime);
 xL_doubledot=num/denom;
 
-
 % solving for normal force; when it's 0, unlatching is done
-value = spring.Force(t,y)-(m_eff*(yL_doubleprime*(x(2)^2)+(xL_doubledot*yL_prime)));
+value = spring_force-(m*(yL_doubleprime*(x(2)^2)+(xL_doubledot*yL_prime)));
+
 
 if (imag(value))
     warning('Complex value of Normal Force. ODE step size might be too large near the end of the latch');
@@ -368,16 +373,74 @@ isterminal=1;
 direction=0;
 end
 
+% calculate m_eff for rotating systems
 function mass = m_eff(load, spring, y0, y)
-y = y(1);
 
-if y > 0
-    y = 0;
+mass = load.mass;
+
+if isa(load, 'RotatingMass')
+    y = y(1);
+    if y > 0
+        y = 0;
+    end
+    L1 = load.L1;
+    theta_0 = load.theta_0;
+    theta = asin((-y+y0+L1*sin(theta_0))/L1);
+    mass = mass/cos(theta) + 1E2*(abs(theta) > pi/2);
 end
 
-y1 = spring.rest_length + abs(y0);
-y2 = spring.rest_length + abs(y);
+mass = mass + spring.mass/3;
 
-mass = load.mass([y1,y2]) + spring.mass/3;
+end
+
+% calculate F_eff for rotating systems
+function Force = F_eff(load, spring, y0, t, y)
+
+Force = spring.Force(t, y);
+
+if isa(load, 'RotatingMass')
+    
+    y1 = spring.rest_length + abs(y0);
+    y2 = spring.rest_length + abs(y(1));
+    theta_0 = load.theta_0;
+    L1 = load.L1;
+    m = load.mass;
+    
+    theta = asin((-y(1)+y0+L1*sin(theta_0))/L1);
+    alpha = acos((y2^2-y1^2+2*y1*L1*sin(theta_0))/(2*y2*L1));
+    
+    Force = real( Force*sin(alpha) - abs(m*sin(theta)*y(2)^2/(L1*cos(theta)^3))) * (abs(theta) < pi/2);
+    Force = Force * (Force > 0);
+end
+
+end
+
+% update force history for standard linear solids
+function status = update_f_unlatching(t, x, ~, spring, latch, y0)
+
+if (isa(spring, 'StandardLinearSolid2') || isa(spring, 'StandardLinearSolid')) && (size(t,2)>0)
+    t = t(end);
+    x = x(:,end);
+    y=(latch.y_L{1}(x(1))-latch.y_L{1}(0))+y0;
+    y_prime = latch.y_L{2}(x(1))*x(2);
+    F = spring.Force(t,[y,y_prime]);
+    spring.F_history = [spring.F_history; [t F]];
+end
+
+status = 0;
+
+end
+
+% update force history for standard linear solids
+function status = update_f_launching(t, y, ~, spring)
+
+if (isa(spring, 'StandardLinearSolid2') || isa(spring, 'StandardLinearSolid')) && (size(t,2)>0)
+    t = t(end);
+    y = y(:,end);
+    F = spring.Force(t,y);
+    spring.F_history = [spring.F_history; [t F]];
+end
+
+status = 0;
 
 end
