0a66788d1966eed56cb8717c45a05872ffa99205
Kaeshav D <kaeshav@gmail.com>
2022-06-17 10:29:16 -0700

    Created Helper Functions

--- lib/solve_lamsa_se.m
+++ lib/solve_lamsa_se.m
@@ -1,8 +1,5 @@
 function [sol,transition_times] = solve_lamsa_se(loading_motor,unlatching_motor,load,latch,spring, outputDirectory)
 %SOLVE_LAMSA_SE Solves equations of motion for series elastic system
-%   
-
-% Section 1: Odeproblem
 
 sol = [0];
 transition_times = [0];
@@ -11,19 +8,45 @@ end
 function dydt = se_ode(t, y, loading_motor, unlatching_motor, load, latch, spring)
 %SE_ODE is the equation of motion for a series elastic system
 %   
-%   y = [theta dot, theta, s dot, s]
+%   y = [theta dot, theta, s dot, s, y1]
 dydt = zeros(4,1);
 
 dydt(1) = 
 dydt(2) = y(1);
 dydt(3) = 
 dydt(4) = y(3);
+dydt(5) = 
 end
 
-function f_perp()
+function f = f_perp()
+%F_PERP is the part of the spring force perpendicular to the lever
 
 end
 
-function alpha()
+function a = alpha()
+%ALPHA is the angle the spring and muscle make with the vertical
+
+end
+
+function f = normal_force()
+%NORMAL_FORCE is the normal force of the latch on the lever
+
+end
+
+function y2 = y2()
+%Y2 is the distance the spring and muscle have contracted
+
+end
+
+function y2dot = y2dot()
+%Y2DOT is the time derivative of y2
+
+end
+
+function y1dot = y1dot()
+%Y1DOT is the velocity of the point of intersection of the muscle and
+%spring along the direction of the muscle.
+% It is calculated using the force-velocity curve of the muscle
+
 
 end
