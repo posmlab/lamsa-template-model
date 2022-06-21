0409e22ce2dd0e79afd1c423f7a259f69e284de0
Kaeshav D <kaeshav@gmail.com>
2022-06-21 12:17:44 -0700

    Changed anonymous function to regular function

--- lib/solve_lamsa_se.m
+++ lib/solve_lamsa_se.m
@@ -11,11 +11,15 @@ function dydt = se_ode(t, y, loading_motor, unlatching_motor, load, latch, sprin
 %   y = [theta dot, theta, s dot, s, y1]
 dydt = zeros(4,1);
 
-moI = load.mass;
-n = normal_force;
+l0 = spring.rest_length + loading_motor.initial_length;
 L1 = load.L1;
 L2 = L1/load.EMA;
-phi = atan(latch.y_L);
+y2 = y2(y(1),  [L1, L2], l0);
+a = alpha(y(1),  y2, l0);
+f_perp = f_perp(alpha, theta);
+moI = load.mass;
+n = normal_force(t, y, unlatching_motor, load, latch, f_perp, [L1, L2]);
+phi = atan(latch.y_L);  % derivative?
 mu = latch.coeff_fric;
 
 dydt(1) = 1/moI * (f_perp*L1 - n*L2*cos(phi) - mu*L2*sin(phi));
@@ -25,28 +29,27 @@ dydt(4) = y(3);
 dydt(5) = y1dot(t, y,loading_motor, spring);
 end
 
-function f = f_perp()
+function f = f_perp(alpha, theta)
 %F_PERP is the part of the spring force perpendicular to the lever
 a = alpha;
     if theta + a >= 90
         f = 0;
     else
-        f = @(theta) spring.Force * sin(pi/2 - theta - a);
+        f = spring.Force * sin(pi/2 - theta - a);
     end
 end 
 
-function a = alpha()
+function a = alpha(theta, y2, l0)
 %ALPHA is the angle the spring and muscle make with the vertical
-l0 = spring.rest_length + loading_motor.initial_length;
 theta0 = load.theta_0;
     if l0 == y2
         a = 0;
     else
-        a = @(theta, y2) asind((cos(theta0) -  cos(theta))/(l0 - y2));
+        a = asind((cos(theta0) -  cos(theta))/(l0 - y2));
     end 
 end
 
-function n = normal_force(t, y, unlatching_motor, load, latch, Fperp)
+function n = normal_force(t, y, unlatching_motor, load, latch, Fperp, lengths)
 %NORMAL_FORCE is the normal force of the latch on the lever
 I = load.moment_of_inertia;
 [~, df, ddf] = latch.f_derivs;
@@ -55,7 +58,7 @@ dsdt = y(4);
 Ful = unlatching_motor.Force(t, [s, dsdt]);
 mL = latch.mass;
 mu = latch.coeff_fric;
-[L1, L2, ~] = load.lengths;
+[L1, L2] = lengths;
 
 phi = atan(df);
 
@@ -64,7 +67,7 @@ n = (I*df*Ful + mL*I*ddf*dsdt*dsdt - mL * Fperp * L1)/( (I*df*mu - mL * L2)*cos(
 end
 
 
-function y2 = y2()
+function y2 = y2(y(1), lengths, l0)
 %Y2 is the distance the spring and muscle have contracted
 
 end
