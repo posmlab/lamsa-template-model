f991d92135b448bcc8933b974a4991de953dd507
Kaeshav D <kaeshav@gmail.com>
2022-06-22 11:10:27 -0700

    Fixed not enough inputs bug

--- test.m
+++ test.m
@@ -1,7 +1,8 @@
 metrics = {'tto', 'vto', 'Pmax', 'ymax', 'tL', 'KEmax', 'yunlatch', 'amax'};
-loading_motor = HillMuscleMotor(.01, 20, 5, 200);
-load = RotatingMassSE(0);
+loading_motor =LinearMotor(5, 5, .1);
+load = RotatingMassSE(0, 0.01, 0.001, 0.001, 0.01, -pi/4);
 unlatching_motor = LinearMotor(10, 10, .005);
 latch = ParabolicLatch(150, .0005, .0003, 0, 0);
-spring = ExponentialSpring(2000, 1e-4);
+spring = ExponentialSpring(200, 1);
+
 [t,y] = solve_lamsa_se(loading_motor,unlatching_motor,load,latch,spring);
