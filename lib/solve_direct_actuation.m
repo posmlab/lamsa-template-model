17efd8cb9c9034175e1df7ffcb68b4468c70ac71
Kaeshav D <kaeshav@gmail.com>
2022-08-03 10:54:09 -0700

    Direct actuation for SE system

--- lib/solve_direct_actuation.m
+++ lib/solve_direct_actuation.m
@@ -1,6 +1,7 @@
 function [sol, transition_times] = solve_direct_actuation(motor, spring, load)
 %Solve differential equation launching phase of direction of direct
-%actuated motion usinf F=ma
+%actuated motion usinf F=ma. Returns [times, y or theta, velocities, force
+% on load]
 %motor struct
     %motor.Force
     %motor.max_force
@@ -10,7 +11,7 @@ function [sol, transition_times] = solve_direct_actuation(motor, spring, load)
 
 
 %% Ballistic phase:F motor only 
-launch_opts=odeset('Events',@(t,y) direct_actuation_end(t,y,motor),'RelTol',1E-7,'AbsTol',1E-10);
+launch_opts=odeset('Events',@(t,y) direct_actuation_end(t,y,motor, load, spring),'RelTol',1E-7,'AbsTol',1E-10);
 ode=@(t,y) direct_actuation_ode(t,y,load,motor,spring);
 
 t_guess_v=(motor.velocity*load.mass)/motor.max_force;
@@ -19,6 +20,9 @@ t_guess_pos=sqrt((2*motor.range*load.mass)/motor.max_force);
 t_guess=max(t_guess_v,t_guess_pos);
 tspan=linspace(0,t_guess,10000);
 y0=[0,0];
+if isa(load, 'RotatingMassSE')
+   y0(1) = load.theta_0; 
+end
 [t,y]=ode45(ode,tspan,y0,launch_opts);
 
 % run ode45 until the projectile launches
@@ -32,7 +36,7 @@ T=[t];
 Y=[y];
 
 for i = 1:size(T)
-    fMotor(i) = motor.Force(T(i), [Y(i,:)]);
+    fMotor(i) = F_eff(load, motor, spring, T(i), [Y(i,:)]);
 end
 fMotor = fMotor';
 
@@ -52,9 +56,21 @@ dy(2)=Force/mass;
 
 end
 
-function [value,isterminal,direction]=direct_actuation_end(t,y,motor)
+function [value,isterminal,direction]=direct_actuation_end(t,y,motor, load, spring)
 %End condition for loading
-value=motor.Force(t,y);
+if isa(load, 'RotatingMassSE')
+    l0 = motor.rest_length + spring.rest_length;
+    L1 = load.lengths(1);
+    theta0 = load.theta_0;
+    beta = sqrt(2*L1^2*(1-cos(y(1)-theta0)) + l0^2 - 2*l0*L1*(sin(y(1))- sin(theta0)));
+    gamma = (L1^2*sin(y(1)-theta0) - l0*L1*cos(y(1)))/beta;
+    y2 = l0 - beta;
+    y2dot = -gamma*y(2);
+
+    value = motor.Force(t, [y2 y2dot]);
+else
+    value=motor.Force(Inf , [y(1), 0]);
+end
 isterminal=1;
 direction=0;
 end
